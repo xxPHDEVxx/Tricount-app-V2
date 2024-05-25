f8c93023d1ef78f14656408f5ff2ccabe9926fb1
rayben <rayanbenaissa@gmail.com>
2024-05-25 04:53:51 +0200

    feat(login): création vue login

---
+++ prbd_2324_a06/Login.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace prbd_2324_a06
+{
+    public partial class Login : Window
+    {
+        public Login() {
+            InitializeComponent();
+        }
+    }
+}
