72c648fb675ccd9d4247794593a396cfbc663698
Jamila H <jamila-h@outlook.fr>
2024-06-02 22:34:23 +0200

    feat(list_tricount): interface graphique de la liste tricount

---
+++ prbd_2324_a06/View/ListView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prbd_2324_a06.View
+{
+    /// <summary>
+    /// Logique d'interaction pour ListView.xaml
+    /// </summary>
+    public partial class ListView : UserControl
+    {
+        public ListView() {
+            InitializeComponent();
+        }
+    }
+}
