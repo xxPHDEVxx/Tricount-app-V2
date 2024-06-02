81307d2641d1a09e764460d54b1eb6bd3a8f71d9
rayben <rayanbenaissa@gmail.com>
2024-06-02 04:08:45 +0200

    login: view

---
+++ prbd_2324_a06/View/LoginView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace prbd_2324_a06
+{
+    public partial class LoginView : UserControl
+    {
+        public LoginView() {
+            InitializeComponent();
+        }
+    }
+}
