43ecd1fb44e4b1fbf151666a5ce008ba9b9b5745
rayben <rayanbenaissa@gmail.com>
2024-06-02 20:01:04 +0200

    Sign up: ajout view + validations + bouton cancel

---
+++ prbd_2324_a06/View/SignUp.xaml.cs
@@ -0,0 +1,17 @@
+﻿using PRBD_Framework;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace prbd_2324_a06.View
+{
+    public partial class SignUp : WindowBase
+    {
+        public SignUp() {
+            InitializeComponent();
+        }
+        
+        private void btnCancel_Click(object sender, RoutedEventArgs e) {
+            Close();
+        }
+    }
+}
