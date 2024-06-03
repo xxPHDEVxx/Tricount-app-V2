00f4724acfa4ccdf085ba3874a6c048dde344059
rayben <rayanbenaissa@gmail.com>
2024-06-03 16:48:45 +0200

    add_operation: création view

---
+++ prbd_2324_a06/View/AddOperationView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace prbd_2324_a06.View
+{
+    public partial class AddOperationView : UserControl
+    {
+        public AddOperationView() {
+            InitializeComponent();
+        }
+    }
+}
