32ab6a3464488116745a68413e98b054dda53e09
rayben <rayanbenaissa@gmail.com>
2024-06-06 17:33:53 +0200

    view_tricount : ajustement view + bindings titre description

--- prbd_2324_a06/ViewModel/ViewTricountViewModel.cs
+++ prbd_2324_a06/ViewModel/ViewTricountViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using prbd_2324_a06.Model;
+using System.Runtime.InteropServices.JavaScript;
 
 namespace prbd_2324_a06.ViewModel
 {
@@ -9,10 +10,28 @@ namespace prbd_2324_a06.ViewModel
         }
 
         private Tricount _tricount;
+        private DateTime _createdAt;
 
         public Tricount Tricount {
             get => _tricount;
             set => SetProperty(ref _tricount, value);
+        }
+
+        public string Title {
+            get => Tricount.Title;
+            set => SetProperty(Tricount.Title, value, Tricount, (tr, t) => {
+            });
+        }
+        public string Description {
+            get => Tricount.Description;
+            set => SetProperty(Tricount.Description, value, Tricount, (tr, d) => {
+            });
+        }
+
+        public DateTime CreatedAt {
+            get => Tricount.CreatedAt;
+            init => SetProperty(Tricount.CreatedAt, value, Tricount
+                , (t, d) => { });
         }
     }
 }
