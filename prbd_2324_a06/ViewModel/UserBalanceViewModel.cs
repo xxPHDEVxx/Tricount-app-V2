636edc8ddc7193b4253ee998f633aef4eb0c01f8
rayben <rayanbenaissa@gmail.com>
2024-06-07 05:55:06 +0200

    fix

--- prbd_2324_a06/ViewModel/UserBalanceViewModel.cs
+++ prbd_2324_a06/ViewModel/UserBalanceViewModel.cs
@@ -10,11 +10,12 @@ namespace prbd_2324_a06.ViewModel
         public UserBalanceViewModel(User user, Tricount tricount) {
             User = user;
             Tricount = tricount;
-          //  UpdateStyle();
+            UpdateStyle();
         }
 
         private User _user;
         private Tricount _tricount;
+
         public User User {
             get => _user;
             set => SetProperty(ref _user, value);
@@ -30,57 +31,56 @@ namespace prbd_2324_a06.ViewModel
             set => SetProperty($"{User.GetMyBalance(Tricount):F2}", value
                 , Tricount, (t, b) => { });
         }
+
         private Brush _backgroundColor;
-        public Brush BackgroundColor
-        {
+
+        public Brush BackgroundColor {
             get => _backgroundColor;
             set => SetProperty(ref _backgroundColor, value);
         }
-        
+
         private int _columnB;
-        public int ColumnB
-        {
+
+        public int ColumnB {
             get => _columnB;
             set => SetProperty(ref _columnB, value);
         }
-        
+
         private int _columnA;
-        public int ColumnA
-        {
+
+        public int ColumnA {
             get => _columnA;
             set => SetProperty(ref _columnA, value);
         }
 
         private HorizontalAlignment _sideA;
-        public HorizontalAlignment SideA
-        {
+
+        public HorizontalAlignment SideA {
             get => _sideA;
             set => SetProperty(ref _sideA, value);
         }
-        
+
         private HorizontalAlignment _sideB;
-        public HorizontalAlignment SideB
-        {
+
+        public HorizontalAlignment SideB {
             get => _sideB;
             set => SetProperty(ref _sideB, value);
         }
-        
-        private void UpdateStyle()
-        {
-            if (int.Parse(Balance) > 0) {
+
+        private void UpdateStyle() {
+            if (int.Parse(Balance) >= 0) {
                 SideA = HorizontalAlignment.Right;
                 SideB = HorizontalAlignment.Left;
-                ColumnA =  0;
+                ColumnA = 0;
                 ColumnB = 1;
                 BackgroundColor = Brushes.LightGreen;
             } else {
                 SideA = HorizontalAlignment.Left;
                 SideB = HorizontalAlignment.Right;
-                ColumnA =  1;
+                ColumnA = 1;
                 ColumnB = 0;
                 BackgroundColor = Brushes.Salmon;
             }
-            
         }
     }
 }
