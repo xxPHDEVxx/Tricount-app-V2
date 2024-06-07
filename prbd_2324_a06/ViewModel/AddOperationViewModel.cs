1308b33ce6ba30c80d48ce15598d401288da6129
rayben <rayanbenaissa@gmail.com>
2024-06-07 23:11:08 +0200

    fix : refresh

--- prbd_2324_a06/ViewModel/AddOperationViewModel.cs
+++ prbd_2324_a06/ViewModel/AddOperationViewModel.cs
@@ -142,6 +142,7 @@ namespace prbd_2324_a06.ViewModel
                 Context.SaveChanges();
                 RaisePropertyChanged();
                 NotifyColleagues(App.Messages.MSG_OPERATION_CHANGED, Operation);
+                NotifyColleagues(App.Messages.MSG_OPERATION_TRICOUNT_CHANGED, Tricount);
                 Close();
             }
         }
