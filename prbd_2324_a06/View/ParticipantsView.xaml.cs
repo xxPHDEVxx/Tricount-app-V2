af68142f8fe976dc9854b38433ba4682c08941e7
Jamila H <jamila-h@outlook.fr>
2024-06-17 00:38:49 +0200

    debut de la carte participant

---
+++ prbd_2324_a06/View/ParticipantsView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prbd_2324_a06.View
+{
+    /// <summary>
+    /// Logique d'interaction pour ParticipantsView.xaml
+    /// </summary>
+    public partial class ParticipantsView : UserControl
+    {
+        public ParticipantsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
